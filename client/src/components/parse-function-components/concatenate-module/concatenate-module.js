abd5576850e2c3e41189336cff4e88bd84aa195e
Michael <michaelmccabe711@gmail.com>
2020-02-15 00:13:48 -0500

    Concatenate module now has appropriate previews.

--- client/src/components/parse-function-components/concatenate-module/concatenate-module.js
+++ client/src/components/parse-function-components/concatenate-module/concatenate-module.js
@@ -18,8 +18,9 @@ class ConcatenateModule extends Component {
 
     handleSubmit = e => {
         e.preventDefault();
-        const { handleModuleCode, id, moduleActiveOff, completeModule } = this.props;
+        const { handleModuleCode, togglePreviewOff, id, moduleActiveOff, completeModule } = this.props;
 
+        togglePreviewOff();
         const moduleCode = "ConcatenateModule";
         handleModuleCode({
             code: moduleCode,
@@ -30,7 +31,77 @@ class ConcatenateModule extends Component {
         moduleActiveOff();
     }
 
+    handlePreview = e => {
+        e.preventDefault();
+        const { previewToggle, togglePreviewOn, togglePreviewOff,
+            outputText, updateDeletionsPreview, updateAdditionsPreview } = this.props;
+
+        let additionPreviews = [];
+        let deletionPreviews = [];
+
+        let inputContainerText
+        for (let i = 0; i < outputText.length; i++) {
+            inputContainerText = outputText[i].text;
+            let outputTextSplitNewLine = inputContainerText.split('\n');
+
+            let createSingleAdditionPreview = outputTextSplitNewLine.map((line, idx) => {
+
+                if (idx === 0) {
+                    // Addition: first index will be plain
+                    return (<span className="line" key={idx}>
+                        <span className="line-number">[1]&#160;</span>
+                        <span className="line-text" style={{ "whiteSpace": "pre-wrap" }}>{line}</span>
+                    </span>)
+
+                } else {
+                    // Addition: concatenate all lines with a green space
+                    return (<span className="line" key={idx}>
+                        <span className="line-text" style={{ background: "rgb(74, 255, 83)", "whiteSpace": "pre-wrap" }}> </span>
+                        <span className="line-text" style={{ "whiteSpace": "pre-wrap" }}>{line}</span>
+                    </span>)
+                }
+            })
+
+            let createSingleDeletionPreview = outputTextSplitNewLine.map((line, idx) => {
+
+                idx = idx + 1;
+
+                if (idx === 1) {
+                    // Deletion: The first index will be plain
+                    return (<div className="line" style={{ "wordWrap": "break-word" }} key={idx}>
+                        <span className="line-number" style={{ float: "left" }}>[{idx}]&#160;</span>
+                        <p className="line-text">{line}</p>
+                    </div>)
+
+                } else if (line === "") {
+                    // Deletion: If line is empty, just display the concatenate-symbol and orange idx
+                    return (<div className="line" key={idx}>
+                        <span className="line-number"
+                            style={{ background: "rgb(250, 217, 155)" }}>[{idx}]&#160;</span>
+                        <span className="concatenate-char" style={{ background: "orange" }}><b>⤻</b></span>
+                    </div>)
+                } else {
+                    // Deletion: If line contains text, display concatenate-symbol at the end and orange idx
+                    return (<div className="line" style={{ "wordWrap": "break-word" }} key={idx}>
+                        <span className="line-number" style={{ background: "rgb(250, 217, 155)", float: "left" }}>[{idx}]&#160;</span>
+                        <span className="concatenate-char" style={{ background: "orange", float: "left" }}><b>⤻</b></span>
+                        <p className="line-text">{line}</p>
+                    </div>)
+                }
+            })
+
+            additionPreviews.push(createSingleAdditionPreview);
+            deletionPreviews.push(createSingleDeletionPreview);
+        }
+        updateDeletionsPreview(deletionPreviews); //  An array of ALL deletion previews
+        updateAdditionsPreview(additionPreviews); // An array of ALL addition previews
+
+        previewToggle === true ? togglePreviewOff() : togglePreviewOn();
+
+    }
+
     render() {
+        const { previewToggle } = this.props;
         return (
             <div className="concatenate-function">
                 <div className="concatenate-card card white">
@@ -43,11 +114,26 @@ class ConcatenateModule extends Component {
                     </div>
                     <div className="row">
                         <div className="concatenate-description col s12">
-                            <p>This module will concatenate all lines of an input into one line.</p>
+                            <p>All lines of an input will be concatenated into one line.</p>
+                            <p>Each concatenated line will be separated by a space.</p>
                         </div>
                     </div>
 
                     <div className="card-action preview-submit">
+
+                        {previewToggle === true ? (
+                            <a
+                                href="!#"
+                                onClick={this.handlePreview}>
+                                Edit Module</a>
+                        ) : (
+                                <a
+                                    href="!#"
+                                    onClick={this.handlePreview}>
+                                    Preview Changes
+                            </a>
+                            )}
+
                         <a
                             href="!#"
                             onClick={this.handleSubmit}
