cdde8ed3fbbd6504d9426330530afe0936ea7ca3
Michael <michaelmccabe711@gmail.com>
2020-02-22 19:24:19 -0500

    necessary heroku information

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const routes = require('./server/routes');
 const http = require("http");
 
 const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 // bodyparser
 app.use(bodyParser.urlencoded({
@@ -16,6 +20,11 @@ const server = http.createServer(app);
 
 // routes
 app.use('/', routes);
+
+// Handles any requests that do not match the ones above
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
